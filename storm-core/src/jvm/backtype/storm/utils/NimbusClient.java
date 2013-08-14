a766cc19469f86508ed9efdb7769a2bc7b93d0cd
anfeng <andy.feng@gmail.com>
2013-08-14 13:45:08 -0500

    Utils.getInt() uses Integer:parseInt(String)

--- storm-core/src/jvm/backtype/storm/utils/NimbusClient.java
+++ storm-core/src/jvm/backtype/storm/utils/NimbusClient.java
@@ -19,26 +19,28 @@ public class NimbusClient extends ThriftClient {
         try {
             String nimbusHost = (String) conf.get(Config.NIMBUS_HOST);
             int nimbusPort = Utils.getInt(conf.get(Config.NIMBUS_THRIFT_PORT));
-                        if (nimbusPort <= 0) {
-                            List<String> zk_hosts = (List<String>) conf.get(Config.STORM_ZOOKEEPER_SERVERS);
-                            Object zk_port = conf.get(Config.STORM_ZOOKEEPER_PORT);
-                            CuratorFramework zk_fw = Utils.newCurator(conf,
-                                    zk_hosts, zk_port,
-                                    (String) conf.get(Config.STORM_ZOOKEEPER_ROOT),
-                                    null); //auth info
-                            zk_fw.start();
-                            try {
-                                byte[] zk_data = zk_fw.getData().forPath("/nimbus");
-                                HostPort nimbus_host_port = (HostPort) Utils.deserialize(zk_data);
-                                LOG.info("Nimbus spec at ZK: " + nimbus_host_port);
-                                nimbusHost = nimbus_host_port.host();
-                                nimbusPort = nimbus_host_port.port();
-                            } catch (Exception e) {
-                                LOG.warn("Failure in obtaining Nimbus host/port from Zookeeper " + zk_hosts + " port " + zk_port, e);
-                            } finally {
-                                zk_fw.close();
-                            }
-                        }
+            if (nimbusPort <= 0) {
+                List<String> zk_hosts = (List<String>) conf.get(Config.STORM_ZOOKEEPER_SERVERS);
+                Object zk_port = conf.get(Config.STORM_ZOOKEEPER_PORT);
+                CuratorFramework zk_fw = Utils.newCurator(conf,
+                        zk_hosts, zk_port,
+                        (String) conf.get(Config.STORM_ZOOKEEPER_ROOT),
+                        null); //auth info
+                zk_fw.start();
+                try {
+                    byte[] zk_data = zk_fw.getData().forPath("/nimbus");
+                    HostPort nimbus_host_port = (HostPort) Utils.deserialize(zk_data);
+                    LOG.debug("Nimbus spec at ZK: " + nimbus_host_port);
+                    nimbusHost = nimbus_host_port.host();
+                    nimbusPort = nimbus_host_port.port();
+                    conf.put(Config.NIMBUS_HOST, nimbusHost);
+                    conf.put(Config.NIMBUS_THRIFT_PORT, (Integer)nimbusPort);
+                } catch (Exception e) {
+                    LOG.warn("Failure in obtaining Nimbus host/port from Zookeeper " + zk_hosts + " port " + zk_port, e);
+                } finally {
+                    zk_fw.close();
+                }
+            }
             return new NimbusClient(conf, nimbusHost, nimbusPort);
         } catch (TTransportException ex) {
             throw new RuntimeException(ex);
