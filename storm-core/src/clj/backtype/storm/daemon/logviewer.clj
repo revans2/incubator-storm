9d3579dba3b04861cd488207023b0a5e043d22fe
Derek Dagit <derekd@yahoo-inc.com>
2013-11-01 21:53:06 +0000

    logviewer and ui authorize nimbus admins

--- storm-core/src/clj/backtype/storm/daemon/logviewer.clj
+++ storm-core/src/clj/backtype/storm/daemon/logviewer.clj
@@ -50,7 +50,9 @@
   (if (or (blank? user) (blank? fname)) 
     nil
     (let [whitelist (get-log-user-whitelist fname)
-          logs-users (concat (*STORM-CONF* LOGS-USERS) whitelist)]
+          logs-users (concat (*STORM-CONF* LOGS-USERS)
+                             (*STORM-CONF* NIMBUS-ADMINS)
+                             whitelist)]
        (some #(= % user) logs-users))))
 
 (defn log-page [fname tail grep user]
