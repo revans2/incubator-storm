26202a8725ebc1f10333a6d601912a8d2813913e
Raghavendra Nandagopal <speaktoraghav@gmail.com>
2014-07-29 17:32:38 -0700

    STORM-348: Netty SASL Authentication

--- storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
+++ storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
@@ -25,6 +25,7 @@
 (deftest test-basic
   (let [req_msg (String. "0123456789abcdefghijklmnopqrstuvwxyz")
         storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM_MESSAGING_NETTY_AUTHENTICATION false
                     STORM-MESSAGING-NETTY-BUFFER-SIZE 1024
                     STORM-MESSAGING-NETTY-MAX-RETRIES 10
                     STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000 
@@ -76,6 +77,7 @@
 (deftest test-large-msg
   (let [req_msg (apply str (repeat 2048000 'c')) 
         storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM_MESSAGING_NETTY_AUTHENTICATION false
                     STORM-MESSAGING-NETTY-BUFFER-SIZE 102400
                     STORM-MESSAGING-NETTY-MAX-RETRIES 10
                     STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000 
@@ -99,6 +101,7 @@
 (deftest test-server-delayed
     (let [req_msg (String. "0123456789abcdefghijklmnopqrstuvwxyz")
        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM_MESSAGING_NETTY_AUTHENTICATION false
                     STORM-MESSAGING-NETTY-BUFFER-SIZE 1024
                     STORM-MESSAGING-NETTY-MAX-RETRIES 10
                     STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000 
@@ -129,6 +132,7 @@
 
 (deftest test-batch
   (let [storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM_MESSAGING_NETTY_AUTHENTICATION false
                     STORM-MESSAGING-NETTY-BUFFER-SIZE 1024000
                     STORM-MESSAGING-NETTY-MAX-RETRIES 10
                     STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000 
