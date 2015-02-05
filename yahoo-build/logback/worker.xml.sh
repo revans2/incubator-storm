18c2568e8808d535b7cfbd83dd521f479b1e7bc9
Robert (Bobby) Evans <evans@yahoo-inc.com>
2015-02-05 15:11:47 -0600

    Updated Yahoo Build

--- yahoo-build/logback/worker.xml.sh
+++ yahoo-build/logback/worker.xml.sh
@@ -30,9 +30,53 @@ cat <<XML
     <suffixPattern>[%p] [\${storm.id}:\${worker.port}] %m%n</suffixPattern>
   </appender>
 
+ <appender name="STDERR" class="ch.qos.logback.core.rolling.RollingFileAppender">
+    <file>\${storm.home}/logs/\${logfile.name}.err</file>
+    <rollingPolicy class="ch.qos.logback.core.rolling.FixedWindowRollingPolicy">
+      <fileNamePattern>\${storm.home}/logs/\${logfile.name}.err.%i.gz</fileNamePattern>
+      <minIndex>1</minIndex>
+      <maxIndex>4</maxIndex>
+    </rollingPolicy>
+
+    <triggeringPolicy class="ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy">
+      <maxFileSize>100MB</maxFileSize>
+    </triggeringPolicy>
+
+    <encoder>
+      <pattern>%m</pattern>
+    </encoder>
+  </appender> 
+
+ <appender name="STDOUT" class="ch.qos.logback.core.rolling.RollingFileAppender">
+    <file>\${storm.home}/logs/\${logfile.name}.out</file>
+    <rollingPolicy class="ch.qos.logback.core.rolling.FixedWindowRollingPolicy">
+      <fileNamePattern>\${storm.home}/logs/\${logfile.name}.out.%i.gz</fileNamePattern>
+      <minIndex>1</minIndex>
+      <maxIndex>4</maxIndex>
+    </rollingPolicy>
+
+    <triggeringPolicy class="ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy">
+      <maxFileSize>100MB</maxFileSize>
+    </triggeringPolicy>
+
+    <encoder>
+      <pattern>%m</pattern>
+    </encoder>
+  </appender> 
+
   <root level="INFO">
    <appender-ref ref="A1"/>
    <appender-ref ref="SYSLOG"/>
   </root>
+
+  <logger name="STDERR" additivity="false">
+    <level value="INFO" />
+    <appender-ref ref="STDERR" />
+  </logger>
+
+  <logger name="STDOUT" additivity="false">
+    <level value="INFO" />
+    <appender-ref ref="STDOUT" />
+  </logger>
 </configuration>
 XML
