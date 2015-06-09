0f52638ad4cbb2974de7d37093f68a006b883e25
Derek Dagit <derekd@yahoo-inc.com>
2015-06-08 20:20:42 -0500

    use serialized value of delay secs for topo actions

--- storm-core/src/clj/backtype/storm/daemon/nimbus.clj
+++ storm-core/src/clj/backtype/storm/daemon/nimbus.clj
@@ -210,7 +210,9 @@
               }
    :killed {:startup (fn [] (delay-event nimbus
                                          storm-id
-                                         (:delay-secs storm-base)
+                                         (-> storm-base
+                                             :topology-action-options
+                                             :delay-secs)
                                          :remove)
                              nil)
             :kill (kill-transition nimbus storm-id)
@@ -222,7 +224,9 @@
             }
    :rebalancing {:startup (fn [] (delay-event nimbus
                                               storm-id
-                                              (:delay-secs storm-base)
+                                              (-> storm-base
+                                                  :topology-action-options
+                                                  :delay-secs)
                                               :do-rebalance)
                                  nil)
                  :kill (kill-transition nimbus storm-id)
