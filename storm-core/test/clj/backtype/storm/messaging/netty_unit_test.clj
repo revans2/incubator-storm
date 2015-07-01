74252d7ea28614ebe12be0bf45aa01eeba085085
Robert (Bobby) Evans <evans@yahoo-inc.com>
2015-07-01 10:36:07 -0500

    Some more netty test updates

--- storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
+++ storm-core/test/clj/backtype/storm/messaging/netty_unit_test.clj
@@ -72,7 +72,8 @@
     (.close client)
     (.close server)
     (.term context)))
-;;
+
+;;TODO need to debug load is not comming back, and it is hanging.
 ;;(deftest test-load
 ;;  (let [req_msg (String. "0123456789abcdefghijklmnopqrstuvwxyz")
 ;;        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
@@ -92,6 +93,7 @@
 ;;        port (available-port 6700)
 ;;        server (.bind context nil port)
 ;;        client (.connect context nil "localhost" port)
+;;        _ (wait-until-ready [server client])
 ;;        _ (.send client task (.getBytes req_msg))
 ;;        iter (.recv server 0 0)
 ;;        resp (.next iter)
@@ -105,37 +107,39 @@
 ;;    (.close client)
 ;;    (.close server)
 ;;    (.term context)))
-;;
-;;(deftest test-large-msg
-;;  (log-message "Should send and receive a large message")
-;;  (let [req_msg (apply str (repeat 2048000 'c'))
-;;        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
-;;                    STORM-MESSAGING-NETTY-AUTHENTICATION false
-;;                    STORM-MESSAGING-NETTY-BUFFER-SIZE 102400
-;;                    STORM-MESSAGING-NETTY-MAX-RETRIES 10
-;;                    STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000
-;;                    STORM-MESSAGING-NETTY-MAX-SLEEP-MS 5000
-;;                    STORM-MESSAGING-NETTY-SERVER-WORKER-THREADS 1
-;;                    STORM-MESSAGING-NETTY-CLIENT-WORKER-THREADS 1
-;;                    TOPOLOGY-KRYO-FACTORY "backtype.storm.serialization.DefaultKryoFactory"
-;;                    TOPOLOGY-TUPLE-SERIALIZER "backtype.storm.serialization.types.ListDelegateSerializer" 
-;;                    TOPOLOGY-FALL-BACK-ON-JAVA-SERIALIZATION false
-;;                    TOPOLOGY-SKIP-MISSING-KRYO-REGISTRATIONS false
-;;                    }
-;;        context (TransportFactory/makeContext storm-conf)
-;;        port (available-port 6700)
-;;        server (.bind context nil port)
-;;        client (.connect context nil "localhost" port)
-;;        _ (wait-until-ready [server client])
-;;        _ (.send client task (.getBytes req_msg))
-;;        iter (.recv server 0 0)
-;;        resp (.next iter)]
-;;    (is (= task (.task resp)))
-;;    (is (= req_msg (String. (.message resp))))
-;;    (.close client)
-;;    (.close server)
-;;    (.term context)))    
-;;    
+
+(deftest test-large-msg
+  (log-message "Should send and receive a large message")
+  (let [req_msg (apply str (repeat 2048000 'c'))
+        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM-MESSAGING-NETTY-AUTHENTICATION false
+                    STORM-MESSAGING-NETTY-BUFFER-SIZE 102400
+                    STORM-MESSAGING-NETTY-MAX-RETRIES 10
+                    STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000
+                    STORM-MESSAGING-NETTY-MAX-SLEEP-MS 5000
+                    STORM-MESSAGING-NETTY-SERVER-WORKER-THREADS 1
+                    STORM-MESSAGING-NETTY-CLIENT-WORKER-THREADS 1
+                    TOPOLOGY-KRYO-FACTORY "backtype.storm.serialization.DefaultKryoFactory"
+                    TOPOLOGY-TUPLE-SERIALIZER "backtype.storm.serialization.types.ListDelegateSerializer" 
+                    TOPOLOGY-FALL-BACK-ON-JAVA-SERIALIZATION false
+                    TOPOLOGY-SKIP-MISSING-KRYO-REGISTRATIONS false
+                    }
+        context (TransportFactory/makeContext storm-conf)
+        port (available-port 6700)
+        server (.bind context nil port)
+        client (.connect context nil "localhost" port)
+        _ (wait-until-ready [server client])
+        _ (.send client task (.getBytes req_msg))
+        iter (.recv server 0 0)
+        resp (.next iter)]
+    (is (= task (.task resp)))
+    (is (= req_msg (String. (.message resp))))
+    (.close client)
+    (.close server)
+    (.term context)))    
+    
+;;TODO test needs to be updated connection now throws away messages when it is not connected
+;; so we need to check status in different situations.
 ;;(deftest test-server-delayed
 ;;    (let [req_msg (String. "0123456789abcdefghijklmnopqrstuvwxyz")
 ;;       storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
@@ -171,51 +175,53 @@
 ;;    (.join server)
 ;;    (.close client)
 ;;    (.term context)))
-;;
-;;
-;;(deftest test-batch
-;;  (let [num-messages 100000
-;;        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
-;;                    STORM-MESSAGING-NETTY-AUTHENTICATION false
-;;                    STORM-MESSAGING-NETTY-BUFFER-SIZE 1024000
-;;                    STORM-MESSAGING-NETTY-MAX-RETRIES 10
-;;                    STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000
-;;                    STORM-MESSAGING-NETTY-MAX-SLEEP-MS 5000
-;;                    STORM-MESSAGING-NETTY-SERVER-WORKER-THREADS 1
-;;                    STORM-MESSAGING-NETTY-CLIENT-WORKER-THREADS 1
-;;                    TOPOLOGY-KRYO-FACTORY "backtype.storm.serialization.DefaultKryoFactory"
-;;                    TOPOLOGY-TUPLE-SERIALIZER "backtype.storm.serialization.types.ListDelegateSerializer"
-;;                    TOPOLOGY-FALL-BACK-ON-JAVA-SERIALIZATION false
-;;                    TOPOLOGY-SKIP-MISSING-KRYO-REGISTRATIONS false
-;;                    }
-;;        _ (log-message "Should send and receive many messages (testing with " num-messages " messages)")
-;;        context (TransportFactory/makeContext storm-conf)
-;;        port (available-port 6700)
-;;        server (.bind context nil port)
-;;        client (.connect context nil "localhost" port)
-;;        _ (wait-until-ready [server client])]
-;;    (doseq [num  (range 1 num-messages)]
-;;      (let [req_msg (str num)]
-;;        (.send client task (.getBytes req_msg))))
-;;
-;;    (let [resp (ArrayList.)
-;;          received (atom 0)]
-;;      (while (< @received (- num-messages 1))
-;;        (let [iter (.recv server 0 0)]
-;;          (while (.hasNext iter)
-;;            (let [msg (.next iter)]
-;;              (.add resp msg)
-;;              (swap! received inc)
-;;              ))))
-;;      (doseq [num  (range 1 num-messages)]
-;;      (let [req_msg (str num)
-;;            resp_msg (String. (.message (.get resp (- num 1))))]
-;;        (is (= req_msg resp_msg)))))
-;;
-;;    (.close client)
-;;    (.close server)
-;;    (.term context)))
-;;
+
+
+(deftest test-batch
+  (let [num-messages 100000
+        storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
+                    STORM-MESSAGING-NETTY-AUTHENTICATION false
+                    STORM-MESSAGING-NETTY-BUFFER-SIZE 1024000
+                    STORM-MESSAGING-NETTY-MAX-RETRIES 10
+                    STORM-MESSAGING-NETTY-MIN-SLEEP-MS 1000
+                    STORM-MESSAGING-NETTY-MAX-SLEEP-MS 5000
+                    STORM-MESSAGING-NETTY-SERVER-WORKER-THREADS 1
+                    STORM-MESSAGING-NETTY-CLIENT-WORKER-THREADS 1
+                    TOPOLOGY-KRYO-FACTORY "backtype.storm.serialization.DefaultKryoFactory"
+                    TOPOLOGY-TUPLE-SERIALIZER "backtype.storm.serialization.types.ListDelegateSerializer"
+                    TOPOLOGY-FALL-BACK-ON-JAVA-SERIALIZATION false
+                    TOPOLOGY-SKIP-MISSING-KRYO-REGISTRATIONS false
+                    }
+        _ (log-message "Should send and receive many messages (testing with " num-messages " messages)")
+        context (TransportFactory/makeContext storm-conf)
+        port (available-port 6700)
+        server (.bind context nil port)
+        client (.connect context nil "localhost" port)
+        _ (wait-until-ready [server client])]
+    (doseq [num  (range 1 num-messages)]
+      (let [req_msg (str num)]
+        (.send client task (.getBytes req_msg))))
+
+    (let [resp (ArrayList.)
+          received (atom 0)]
+      (while (< @received (- num-messages 1))
+        (let [iter (.recv server 0 0)]
+          (while (.hasNext iter)
+            (let [msg (.next iter)]
+              (.add resp msg)
+              (swap! received inc)
+              ))))
+      (doseq [num  (range 1 num-messages)]
+      (let [req_msg (str num)
+            resp_msg (String. (.message (.get resp (- num 1))))]
+        (is (= req_msg resp_msg)))))
+
+    (.close client)
+    (.close server)
+    (.term context)))
+
+;;TODO test needs to be updated this is not really true any more
+;; we might just want to delete this test.
 ;;(deftest test-server-always-reconnects
 ;;    (let [req_msg (String. "0123456789abcdefghijklmnopqrstuvwxyz")
 ;;       storm-conf {STORM-MESSAGING-TRANSPORT "backtype.storm.messaging.netty.Context"
