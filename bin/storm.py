7326f5352734625f03af8978ee56e7fa3146170b
zhuol <zhuol@yahoo-inc.com>
2015-07-08 13:58:14 -0500

    YSTORM-1614, implement kill_workers CLI for Summerfix
    Add comments about secure mode

--- bin/storm.py
+++ bin/storm.py
@@ -367,6 +367,19 @@ def get_errors(*args):
         jvmtype="-client",
         extrajars=[USER_CONF_DIR, os.path.join(STORM_DIR, "bin")])
 
+def kill_workers(*args):
+    """Syntax: [storm kill_workers]
+
+    Kill the workers running on this supervisor. This command should be run
+    on a supervisor node. If the cluster is running in secure node, then user needs 
+    to have admin rights on the node to be able to successfully kill all workers.
+    """
+    exec_storm_class(
+        "backtype.storm.command.kill_workers",
+        args=args,
+        jvmtype="-client",
+        extrajars=[USER_CONF_DIR, os.path.join(STORM_DIR, "bin")])
+
 def shell(resourcesdir, command, *args):
     tmpjarpath = "stormshell" + str(random.randint(0, 10000000)) + ".jar"
     os.system("jar cf %s %s" % (tmpjarpath, resourcesdir))
@@ -582,7 +595,9 @@ COMMANDS = {"jar": jar, "kill": kill, "shell": shell, "nimbus": nimbus, "ui": ui
             "activate": activate, "deactivate": deactivate, "rebalance": rebalance, "help": print_usage,
             "list": listtopos, "dev-zookeeper": dev_zookeeper, "version": version, 
             "upload-credentials": upload_credentials, "blobstore": blobstore, "pacemaker": pacemaker,
-            "heartbeats": heartbeats, "set_log_level": set_log_level, "get-errors": get_errors }
+            "heartbeats": heartbeats, "set_log_level": set_log_level, "get-errors": get_errors,
+            "kill_workers": kill_workers
+            }
 
 def parse_config(config_list):
     global CONFIG_OPTS
