bfd3a1ee8d02b6eb51f51aee5b11d392e3bb1204
Boyang Jerry Peng <jerrypeng@yahoo-inc.com>
2015-11-18 11:07:36 -0600

    edits based on comments

--- storm-core/src/jvm/backtype/storm/scheduler/resource/strategies/ResourceAwareStrategy.java
+++ storm-core/src/jvm/backtype/storm/scheduler/resource/strategies/ResourceAwareStrategy.java
@@ -262,7 +262,7 @@ public class ResourceAwareStrategy implements IStrategy {
     }
 
     private Double distToNode(RAS_Node src, RAS_Node dest) {
-        if (src.getId().equals(dest.getId()) == true) {
+        if (src.getId().equals(dest.getId())) {
             return 0.0;
         }else if (this.NodeToCluster(src) == this.NodeToCluster(dest)) {
             return 0.5;
