f92f6bd4130eed1f1a82d3a3fd997cfc99fcbcdb
Thomas Graves <tgraves@yahoo-inc.com>
2016-01-22 22:57:12 +0000

    Make changes to support both kafka 0.8.3 security and 0.9 with changes to SimpleConsumer api

--- external/storm-kafka/src/jvm/storm/kafka/DynamicPartitionConnections.java
+++ external/storm-kafka/src/jvm/storm/kafka/DynamicPartitionConnections.java
@@ -22,6 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.kafka.trident.IBrokerReader;
 
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -44,10 +45,43 @@ public class DynamicPartitionConnections {
     Map<Broker, ConnectionInfo> _connections = new HashMap();
     KafkaConfig _config;
     IBrokerReader _reader;
+    Constructor<SimpleConsumer> _simpleConsumerConstr;
+    boolean useSecurityParamSimpleConsumer = false;
 
     public DynamicPartitionConnections(KafkaConfig config, IBrokerReader brokerReader) {
         _config = config;
         _reader = brokerReader;
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        try {
+            Class c = loader.loadClass("kafka.javaapi.consumer.SimpleConsumer");
+
+            try {
+                // This version of the constructor exists in our early Security version in 
+                // 0.8.x and is here to keep backwards compatibility. If this constructor 
+                // exists use it, otherwise fallback.
+                LOG.debug("Try using SimpleConsumer api with security");
+                Constructor[] clist =  c.getConstructors();
+                for (int i = 0; i < clist.length; i++) {
+                   LOG.info("constructor " + i + " is: " + clist[i].toString());
+                }
+                _simpleConsumerConstr = c.getDeclaredConstructor(String.class, int.class, int.class, 
+                    int.class, String.class, String.class);
+                useSecurityParamSimpleConsumer = true;
+            } catch (NoSuchMethodException e) {
+                try {
+                    // This is the constructor that exists in the open source kafka 0.8 
+                    // and 0.9 versions
+                    LOG.debug("Fall back to using open source SimpleConsumer api");
+                     _simpleConsumerConstr = c.getDeclaredConstructor(String.class, int.class, 
+                         int.class, int.class, String.class);
+                     useSecurityParamSimpleConsumer = false;
+                } catch (NoSuchMethodException ne) {
+                    LOG.error("Error finding constructor for kafka.javaapi.consumer.SimpleConsumer");
+                }
+            }
+        } catch (ClassNotFoundException cne) {
+            LOG.error("Error finding class kafka.javaapi.consumer.SimpleConsumer");
+        }
     }
 
     public SimpleConsumer register(Partition partition) {
@@ -57,7 +91,20 @@ public class DynamicPartitionConnections {
 
     public SimpleConsumer register(Broker host, int partition) {
         if (!_connections.containsKey(host)) {
-            _connections.put(host, new ConnectionInfo(new SimpleConsumer(host.host, host.port, _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId, _config.securityProtocol)));
+            SimpleConsumer sconsumer; 
+            try {
+                if (useSecurityParamSimpleConsumer) {
+                    sconsumer = _simpleConsumerConstr.newInstance(host.host, host.port, 
+                        _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId, 
+                        _config.securityProtocol);
+                } else {
+                    sconsumer = _simpleConsumerConstr.newInstance(host.host, host.port, 
+                        _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId);
+                }
+                _connections.put(host, new ConnectionInfo(sconsumer));
+            } catch (Exception ie) {
+                LOG.error("Error creating new instance of kafka.javaapi.consumer.SimpleConsumer");
+            }
         }
         ConnectionInfo info = _connections.get(host);
         info.partitions.add(partition);
--- external/storm-kafka/src/jvm/storm/kafka/StaticPartitionConnections.java
+++ external/storm-kafka/src/jvm/storm/kafka/StaticPartitionConnections.java
@@ -18,14 +18,22 @@
 package storm.kafka;
 
 import kafka.javaapi.consumer.SimpleConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StaticPartitionConnections {
+
+    public static final Logger LOG = LoggerFactory.getLogger(StaticPartitionConnections.class);
+
     Map<Integer, SimpleConsumer> _kafka = new HashMap<Integer, SimpleConsumer>();
     KafkaConfig _config;
     StaticHosts hosts;
+    Constructor<SimpleConsumer> _simpleConsumerConstr;
+    boolean useSecurityParamSimpleConsumer = false;
 
     public StaticPartitionConnections(KafkaConfig conf) {
         _config = conf;
@@ -33,13 +41,52 @@ public class StaticPartitionConnections {
             throw new RuntimeException("Must configure with static hosts");
         }
         this.hosts = (StaticHosts) conf.hosts;
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        try {
+            Class c = loader.loadClass("kafka.javaapi.consumer.SimpleConsumer");
+
+            try {
+                // This version of the constructor exists in our early Security version in 
+                // 0.8.x and is here to keep backwards compatibility. If this constructor 
+                // exists use it, otherwise fallback.
+                _simpleConsumerConstr = c.getDeclaredConstructor(String.class, int.class, int.class,
+                    int.class, String.class,
+                    org.apache.kafka.common.protocol.SecurityProtocol.class);
+                useSecurityParamSimpleConsumer = true;
+            } catch (NoSuchMethodException e) {
+                try {
+                    // This is the constructor that exists in the open source kafka 0.8 
+                    // and 0.9 versions
+                     _simpleConsumerConstr = c.getDeclaredConstructor(String.class, int.class, int.class,
+                         int.class, String.class);
+                     useSecurityParamSimpleConsumer = false;
+                } catch (NoSuchMethodException ne) {
+                    LOG.error("Error finding constructor for kafka.javaapi.consumer.SimpleConsumer");
+                }
+            }
+        } catch (ClassNotFoundException cne) {
+            LOG.error("Error finding class kafka.javaapi.consumer.SimpleConsumer");
+        }
+
     }
 
     public SimpleConsumer getConsumer(int partition) {
         if (!_kafka.containsKey(partition)) {
             Broker hp = hosts.getPartitionInformation().getBrokerFor(partition);
-            _kafka.put(partition, new SimpleConsumer(hp.host, hp.port, _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId, _config.securityProtocol));
-
+           SimpleConsumer sconsumer;
+            try {
+                if (useSecurityParamSimpleConsumer) {
+                    sconsumer = _simpleConsumerConstr.newInstance(hp.host, hp.port,
+                        _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId,
+                        _config.securityProtocol);
+                } else {
+                    sconsumer = _simpleConsumerConstr.newInstance(hp.host, hp.port,
+                        _config.socketTimeoutMs, _config.bufferSizeBytes, _config.clientId);
+                }
+                _kafka.put(partition, sconsumer);
+            } catch (Exception ie) {
+                LOG.error("Error creating new instance of kafka.javaapi.consumer.SimpleConsumer");
+            }
         }
         return _kafka.get(partition);
     }
