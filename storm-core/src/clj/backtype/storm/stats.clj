02071fe0d69a514903bc09e1042addef1d443598
zhuol <zhuol@yahoo-inc.com>
2016-04-06 13:58:38 -0500

    Minor

--- storm-core/src/clj/backtype/storm/stats.clj
+++ storm-core/src/clj/backtype/storm/stats.clj
@@ -408,7 +408,7 @@
       ((fn [[weighted-avg cnt]]
          (if (> uptime 0)
            (div weighted-avg (* 1000 (min uptime 600)))
-           0))))))
+           0.))))))
 
 (defn agg-pre-merge-comp-page-bolt
   [{exec-id :exec-id
