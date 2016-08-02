190ed25ba5e55e1229157d287eadc83d20789d3d
Kishor Patil <kpatil@yahoo-inc.com>
2016-08-02 22:45:10 +0000

    Revert "Merge pull request #789 from ppoulosk/YSTORM-3095"
    This reverts commit a9403584f98e5f2d42ea2084d177238ce8848f6b, reversing
    changes made to a8a63a8a525ff8450a07f417f64ac3e0cab8af85.

--- storm-core/src/jvm/backtype/storm/scheduler/resource/ResourceAwareScheduler.java
+++ storm-core/src/jvm/backtype/storm/scheduler/resource/ResourceAwareScheduler.java
@@ -22,7 +22,6 @@ import backtype.storm.Config;
 import backtype.storm.scheduler.resource.strategies.eviction.IEvictionStrategy;
 import backtype.storm.scheduler.resource.strategies.priority.ISchedulingPriorityStrategy;
 import backtype.storm.scheduler.resource.strategies.scheduling.IStrategy;
-import backtype.storm.scheduler.utils.IConfigLoader;
 import backtype.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,7 +29,6 @@ import org.slf4j.LoggerFactory;
 import backtype.storm.scheduler.Cluster;
 import backtype.storm.scheduler.ExecutorDetails;
 import backtype.storm.scheduler.IScheduler;
-import backtype.storm.scheduler.utils.SchedulerUtils;
 import backtype.storm.scheduler.Topologies;
 import backtype.storm.scheduler.TopologyDetails;
 import backtype.storm.scheduler.WorkerSlot;
@@ -48,7 +46,6 @@ public class ResourceAwareScheduler implements IScheduler {
 
     @SuppressWarnings("rawtypes")
     private Map conf;
-    private IConfigLoader configLoader;
 
     private static final Logger LOG = LoggerFactory
             .getLogger(ResourceAwareScheduler.class);
@@ -56,7 +53,7 @@ public class ResourceAwareScheduler implements IScheduler {
     @Override
     public void prepare(Map conf) {
         this.conf = conf;
-        this.configLoader = SchedulerUtils.getConfigLoader(conf, Config.RESOURCE_AWARE_SCHEDULER_USER_POOLS_LOADER);
+
     }
 
     @Override
@@ -392,15 +389,6 @@ public class ResourceAwareScheduler implements IScheduler {
      * {userid->{resourceType->amountGuaranteed}}
      */
     private Map<String, Map<String, Double>> getUserResourcePools() {
-        // If loader plugin is configured, then try it
-        if (this.configLoader != null) {
-            Map<String, Map<String, Double>> ret = configLoader.load();
-            if (ret != null) {
-                return ret;
-            }
-        }
-
-        // Fall back to standard configs if it did not return a usable map
         Object raw = this.conf.get(Config.RESOURCE_AWARE_SCHEDULER_USER_POOLS);
         Map<String, Map<String, Double>> ret = new HashMap<String, Map<String, Double>>();
 
