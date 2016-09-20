d303dd772d9d71dda4b712c6e28b9a042e534788
Alessandro Bellina <abellina@yahoo-inc.com>
2016-09-20 08:27:54 -0500

    YSTORM-3712: the NPE in compute-executors is due to the component->executors map being nil

--- storm-core/src/clj/backtype/storm/daemon/nimbus.clj
+++ storm-core/src/clj/backtype/storm/daemon/nimbus.clj
@@ -550,15 +550,15 @@
         storm-conf (read-storm-conf-as-nimbus conf storm-id blob-store)
         topology (read-storm-topology-as-nimbus storm-id blob-store)
         task->component (storm-task-info topology storm-conf)]
-    (or
-     (some->> (storm-task-info topology storm-conf)
-              reverse-map
-              (map-val sort)
-              (join-maps component->executors)
-              (map-val (partial apply partition-fixed))
-              (mapcat second)
-              (map to-executor-id))
-     [])))
+    (if (nil? component->executors)
+      []
+      (->> (storm-task-info topology storm-conf)
+           reverse-map
+           (map-val sort)
+           (join-maps component->executors)
+           (map-val (partial apply partition-fixed))
+           (mapcat second)
+           (map to-executor-id)))))
 
 (defn- compute-executor->component [nimbus storm-id]
   (let [conf (:conf nimbus)
