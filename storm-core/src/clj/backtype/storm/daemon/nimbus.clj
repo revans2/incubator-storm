273df7f0e6893c9794e93b6127ae4991856d2417
Kyle Nusbaum <knusbaum@yahoo-inc.com>
2016-11-02 15:44:48 -0500

    Cleaning up some of worker-launcher

--- storm-core/src/clj/backtype/storm/daemon/nimbus.clj
+++ storm-core/src/clj/backtype/storm/daemon/nimbus.clj
@@ -1484,6 +1484,7 @@
           (check-authorization! nimbus storm-name nil "submitTopology")
           (check-storm-active! nimbus storm-name false)
           (let [topo-conf (from-json serializedConf)]
+            (log-message "TOPO-CONFIG is: " topo-conf)
             (try
               (validate-configs-with-schemas topo-conf)
               (catch IllegalArgumentException ex
