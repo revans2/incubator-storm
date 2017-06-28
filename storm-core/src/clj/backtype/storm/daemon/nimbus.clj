2fa4a3f501fdbfd6e1b354882a500710c14fd518
Kyle Nusbaum <knusbaum@yahoo-inc.com>
2017-06-28 10:37:21 -0500

    Adding logging for the scheduler

--- storm-core/src/clj/backtype/storm/daemon/nimbus.clj
+++ storm-core/src/clj/backtype/storm/daemon/nimbus.clj
@@ -888,7 +888,9 @@
     (.setStatusMap cluster (deref (:id->sched-status nimbus)))
     ;; call scheduler.schedule to schedule all the topologies
     ;; the new assignments for all the topologies are in the cluster object.
-    (.schedule (:scheduler nimbus) topologies cluster)
+    (let [before-schedule (current-time-millis)]
+      (.schedule (:scheduler nimbus) topologies cluster)
+      (log-message "Scheduling took " (- (current-time-millis) before-schedule) " ms."))
 
     ;;merge with existing statuses
     (reset! (:id->sched-status nimbus) (merge (deref (:id->sched-status nimbus)) (.getStatusMap cluster)))
