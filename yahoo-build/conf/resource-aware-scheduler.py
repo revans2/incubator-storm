303b8d28344b115863ffdb9d276a838fa602e73a
Bobby Evans <evans@yahoo-inc.com>
2017-09-19 09:29:03 -0500

    YSTORM-4578: stop using yaml module

--- yahoo-build/conf/resource-aware-scheduler.py
+++ yahoo-build/conf/resource-aware-scheduler.py
@@ -6,13 +6,39 @@
 
 import os
 import json
-import yaml
+import types
 
 print """
 # This configuration file is controlled by yinst set variables.
 # This is for the resource-aware-scheduler
 
 """
+
+def toYml(data, indent):
+    dt = type(data)
+    if dt is types.NoneType:
+        ret = "null"
+    elif dt is types.BooleanType:
+        if data:
+            ret = "true"
+        else:
+            ret = "false"
+    elif dt is types.IntType or dt is types.LongType or dt is types.FloatType:
+        ret = str(data)
+    elif dt is types.StringType or dt is types.UnicodeType:
+        ret = "\"" + data.replace("\\","\\\\").replace("\"","\\\"") + "\""
+    elif dt is types.TupleType or dt is types.ListType:
+        ret = "\n"
+        for part in data:
+            ret += "    " * indent + "- " + toYml(part, indent+1)+"\n"
+    elif dt is types.DictType:
+        ret = "\n"
+        for k in sorted(data.iterkeys()):
+            v = data[k]
+            ret += "    " * indent + k + ": "+ toYml(v, indent+1)+"\n"
+    else:
+        raise "Don't know how to convert %s to YAML type is %s"%(data, dt)
+    return ret
 
 user_resource_pool_key = "resource.aware.scheduler.user.pools"
 
@@ -29,7 +55,7 @@ if resource_pool_json is not None:
 
     yml = "";
     try:
-        yml = yaml.safe_dump(resource_pool, default_flow_style=False)
+        yml = toYml(resource_pool, 0)
     except:
         print "Error occurred in converting to YAML!"
         raise
