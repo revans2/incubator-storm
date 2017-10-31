953c01b94460c7d79e2fece30bcf4abe147ee28d
Kishor Patil <kpatil@yahoo-inc.com>
2017-10-31 15:13:44 -0400

    ArtifactoryConfigLoader should be able to accept https scheme

--- storm-core/src/jvm/backtype/storm/scheduler/utils/ArtifactoryConfigLoader.java
+++ storm-core/src/jvm/backtype/storm/scheduler/utils/ArtifactoryConfigLoader.java
@@ -46,7 +46,7 @@ import org.yaml.snakeyaml.constructor.SafeConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ArtifactoryConfigLoader implements IConfigLoader {
+    public class ArtifactoryConfigLoader implements IConfigLoader {
     /**
      * Configuration items for this config loader are passed in via confg settings in 
      * each scheduler that has a configurable loader.
@@ -315,7 +315,7 @@ public class ArtifactoryConfigLoader implements IConfigLoader {
             try {
                 uri = new URI(uriString);
                 myScheme = uri.getScheme().toLowerCase();
-                if (myScheme.equals("http")) {
+                if (myScheme.equals("http") || myScheme.equals("https")) {
                     host = uri.getHost();
                     port = uri.getPort();
                     location = uri.getPath();
